22b90ebf92225f2f001da15327d6579525367c9a
Nikky Southerland <nikky@uw.edu>
2017-06-19 21:11:07 -0700

    Checkpoint

--- app/services/article_updater.rb
+++ app/services/article_updater.rb
@@ -7,15 +7,17 @@ class ArticleUpdater
 
   def retrieve_and_update
     feed.entries.each do |article|
-      puts "-> #{article.title}"
-      puts "-> #{sanitize article.summary, tags: []}"
-      puts "-> #{article.published}"
-      puts "-> #{article.author}"
-      puts "-> #{article.entry_id}"
-      article_record = Article.where(guid: article.entry_id).order('id desc').last
+      article_record = Article.where(guid: article.entry_id).order('id desc').first
+      summary =  sanitize(article.summary, tags: [])
       if article_record
-        puts "--> Title changed!" if article_record.title != article.title
-        puts "--> Summary changed!" if article_record.description != article.summary
+        if article_record.title != article.title
+          puts "--> Title changed‘’!"
+          puts "--> Old title: #{article_record.title}. New title: #{article.title}"
+        end
+        if article_record.description != summary
+          puts "--> Summary changed!"
+          puts "--> Old summary: #{article_record.description}. New title: #{summary}"
+        end
       end
       persist_article(article)
     end
